7b29af3212b166c631c49ed6d6ed3627dcb2e991
cotitech <eli@coti.io>
2022-03-14 11:46:35 +0200

    uiIdle moved to ui_flow

--- app/src/ui_flow.h
+++ app/src/ui_flow.h
@@ -6,3 +6,5 @@
 extern const ux_flow_step_t *const ux_idle_flow[];
 extern const ux_flow_step_t *const ux_display_public_flow[];
 extern const ux_flow_step_t *const ux_sign_flow[];
+
+void uiIdle(void);
